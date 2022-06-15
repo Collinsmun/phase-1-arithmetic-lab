b9fff3c3e9acd6b7e62684ec5ae8b92373247f03
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-15 18:05:10 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+// multiply
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
+console.log(multiply);
+
+// random number
+const random = Math.floor(Math.random()*10)+1;
+console.log(random);
+
+// mod
+let num3=13;
+let num4=9;
+const mod = num3%num4;
+console.log(mod)
+
+// max
+const max =Math.max(1,2,3,4,12,13,15,17,19,20);
+console.log(max);
